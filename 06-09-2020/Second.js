b924b199e962e7f1b786f324c32ed20695ebb4eb
renuka2009 <70653359+renuka2009@users.noreply.github.com>
2020-09-05 22:14:17 -0700

    Create Second.js

---
+++ 06-09-2020/Second.js
@@ -0,0 +1,8 @@
+function PrintFirstElement (A)
+{
+    return A[0]
+}
+
+console.log(PrintFirstElement([1,2,3,4,5]))
+console.log(PrintFirstElement([[1,2,3],4,5,6,[9,10],122]))
+console.log(PrintFirstElement([[1,2,3],[4,5,6],[7,8,9]]))
