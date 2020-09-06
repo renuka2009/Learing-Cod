3ac166c7440d668ec70937578cd3445df3f4b280
renuka2009 <70653359+renuka2009@users.noreply.github.com>
2020-09-05 22:22:24 -0700

    Create Third.js

---
+++ 06-09-2020/Third.js
@@ -0,0 +1,8 @@
+function ChangeFirstElement (A,value)
+{
+    return  A[0]=value
+}
+
+console.log(ChangeFirstElement([1,2,3,4,5],22))
+console.log(ChangeFirstElement([[1,2,3],4,5,6,[9,10],122],23))
+console.log(ChangeFirstElement([[1,2,3],[4,5,6],[7,8,9]],25))
