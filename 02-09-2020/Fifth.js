9588cd74ce5cf64517435e16215cf17da7087016
renuka2009 <70653359+renuka2009@users.noreply.github.com>
2020-09-06 07:25:43 -0700

    Create Fifth.js

---
+++ 02-09-2020/Fifth.js
@@ -0,0 +1,146 @@
+function  isDivisibililiyOne (num)
+{
+    if(num%1==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyOne(13))
+console.log(isDivisibililiyOne(15))
+
+function isDivisibililiyTwo (num)
+{
+    if(num%2==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyTwo(34))
+console.log(isDivisibililiyTwo(13))
+
+function isDivisibililiyThree (num)
+{
+    if(num%3==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyThree(30))
+console.log(isDivisibililiyThree(25))
+
+function isDivisibililiyFour (num)
+{
+    if(num%4==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyFour(39))
+console.log(isDivisibililiyFour(40))
+
+function isDivisibililiyFive (num)
+{
+    if(num%5==0)
+    {
+        return  true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyFive(20))
+console.log(isDivisibililiyFive(39))
+
+function isDivisibililiysix (num)
+{
+    if(num%6==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiysix(23))
+console.log(isDivisibililiysix(36))
+
+function isDivisibililiyseveen (num)
+{
+    if(num%7==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyseveen(13))
+console.log|(isDivisibililiyseveen(14))
+
+function isDivisibililiyEight (num)
+{
+    if(num%8==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyEight(89))
+console.log(isDivisibililiyEight(80))
+
+function isDivisibililiyNine (num)
+{
+    if(num%9==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isDivisibililiyNine(35))
+console.log(isDivisibililiyNine(81))
+
+function isDivisibililiyTen (num)
+{
+    if(num%2==0 && num%5==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
