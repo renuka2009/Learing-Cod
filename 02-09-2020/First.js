4b40ac3937362864d6280c813b44e1844bca7658
renuka2009 <70653359+renuka2009@users.noreply.github.com>
2020-09-06 05:41:21 -0700

    Create First.js

---
+++ 02-09-2020/First.js
@@ -0,0 +1,14 @@
+function isEeven (num)
+{
+    if (num%2==0)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(isEeven(14))
+console.log(isEeven(13))
