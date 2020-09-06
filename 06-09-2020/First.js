e7f8be86d637e5fee8c42c874830f744c7398a09
renuka2009 <70653359+renuka2009@users.noreply.github.com>
2020-09-05 21:17:33 -0700

    Create First.js

---
+++ 06-09-2020/First.js
@@ -0,0 +1,7 @@
+function printLastname (A)
+{
+   return  A [A.length-1]  
+}
+console.log(printLastname([1,2,3,4,5]))
+console.log(printLastname([[1,2,3],4,5,6,[9,10],122]))
+console.log(printLastname([[1,2,3],[4,5,6],[7,8,9]]))
