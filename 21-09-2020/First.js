f8b3acc390d059d7c60d96c26460db8ca8c82989
renuka2009 <70653359+renuka2009@users.noreply.github.com>
2020-09-21 17:08:22 +0530

    Create First.js

---
+++ 21-09-2020/First.js
@@ -0,0 +1,8 @@
+function printSmallestArrayElement(A)
+{
+   A.sort 
+    return A[0]
+}
+
+console.log(printSmallestArrayElement([3,4,5,1,5,6,10,12,14,19,22,25]))
+console.log(printSmallestArrayElement([1,2,48,19,,16,110,212,314,419,522,125]))
